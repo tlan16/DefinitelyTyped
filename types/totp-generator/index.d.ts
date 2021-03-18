0a1150c7b061df346fa76f8922849108de7eca2e
Frank Lan <flan@msts.com>
2021-03-19 00:51:49 +1100

    add author info

--- types/totp-generator/index.d.ts
+++ types/totp-generator/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for totp-generator 0.0.8
+// Project: https://github.com/bellstrand/totp-generator
+// Definitions by: Frank Lan <https://github.com/tlan16>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
 declare module "totp-generator" {
     function OTP(secret: string): string;
     export = OTP;
